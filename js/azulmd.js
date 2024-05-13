0b4d8d524ef9a014ca33f050c961bfc5efb0bb73
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-05-13 17:31:57 +0200

    initial commit

---
+++ js/azulmd.js
@@ -0,0 +1,29 @@
+const frag= {typ:"div", style:{margin:"20px", listStylePosition: "inside",},ch:[
+{typ:"h5",id:"header5",ch:[
+{typ:"txt", txt:`Header5`},
+]},
+{typ:"p",ch:[
+{typ:"txt", txt:`You may also want some images right in here like `},
+{typ:"img",src:"https://cloud.githubusercontent.com/assets/5456665/13322882/e74f6626-dc00-11e5-921d-f6d024a01eaa.png",ch:[
+{typ:"txt", txt:`GitHub Logo`},
+]},
+{typ:"txt", txt:` - you can do that but I would recommend you to use the component "image" and simply split your text.`},
+{typ:"txt", txt:`
+`},]},
+{typ:"h6",id:"header6",ch:[
+{typ:"txt", txt:`Header6`},
+]},
+{typ:"p",ch:[
+{typ:"txt", txt:`Let us do some links - this for example: `},
+{typ:"a",href:"https://github.com/MinhasKamal/github-markdown-syntax",ch:[
+{typ:"txt", txt:`https://github.com/MinhasKamal/github-markdown-syntax`},
+]},
+{typ:"txt", txt:` is `},
+{typ:"span",style:{fontWeight:"bold",}, ch:[{typ:"txt", txt:`NOT`},
+]},{typ:"txt", txt:` a link but this: is `},
+{typ:"a",href:"https://github.com/MinhasKamal/github-markdown-syntax",ch:[
+{typ:"txt", txt:`GitHub`},
+]},
+{typ:"txt", txt:`
+`},]},
+],};
